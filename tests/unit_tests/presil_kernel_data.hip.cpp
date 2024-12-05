ee28fcbabd3fdb4bf51ea1fec81f50dd9baa8e75
Abhishek Mishra <abhishek.mishra2@amd.com>
2024-12-05 16:34:04 +0000

    updated the presil_kernel_data test; uses c++ vectors for data

--- tests/unit_tests/presil_kernel_data.hip.cpp
+++ tests/unit_tests/presil_kernel_data.hip.cpp
@@ -22,7 +22,7 @@ template<unsigned int elements_per_lane, unsigned int chunks_per_block, typename
 int main(int argc, char* argv[]) {
 
     hipError_t    hip_error;
-    hipKernelInfo kernel_data;
+    hipKernelData kernel_data;
 
     if (argc == 1) {
         std::cout << "No arch specified! Please specify an arch (e.g., gfx1100)" << std::endl;
@@ -31,42 +31,43 @@ int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; ++i) {
 
-        hip_error = hipGetKernelData((void *) triad_kernel<2,1,float>, &kernel_data, argv[i]);
+        hip_error = hipGetKernelData((void *) triad_kernel<2,1,float>, argv[i], kernel_data);
 
         if (hip_error != hipSuccess) {
             std::cout << "Ran with DEFAULT HIP. Pre-Sil HIP .so NOT used!" << std::endl;
             return 0;
         }
         std::cout << "\nArch: " << argv[i] << std::endl;
-        printf("kernel binary size: %zu \n", kernel_data.binary.size);
+        printf("kernel binary size: %zu \n", kernel_data.kernelBin.size());
         printf("kernel binary:\n");
-        for (int i = 0; i < kernel_data.binary.size; i++) {
-            printf("%i ", kernel_data.binary.data[i]);
+        for (int i = 0; i < kernel_data.kernelBin.size(); i++) {
+            printf("%i ", kernel_data.kernelBin[i]);
         }
-        printf("\nargs: %zu", kernel_data.kernArgsSizes.size);
-        for (size_t i = 0; i < kernel_data.kernArgsSizes.size; ++i) {
-            printf("\n - .size:\t%i\n", kernel_data.kernArgsSizes.data[i]);
-            printf("   .offset:\t%i\n", kernel_data.kernArgsOffsets.data[i]);
-            if (i < kernel_data.kernArgsAccQualifiers.size) {
-                switch (kernel_data.kernArgsAccQualifiers.data[i])
+        printf("\nargs: %zu", kernel_data.kArgsSizes.size());
+        for (size_t i = 0; i < kernel_data.kArgsSizes.size(); ++i) {
+            printf("\n - .size:\t%i\n", kernel_data.kArgsSizes[i]);
+            printf("   .offset:\t%i\n", kernel_data.kArgsOffsets[i]);
+            if (i < kernel_data.kArgsAccQuals.size()) {
+                switch (kernel_data.kArgsAccQuals[i])
                 {
-                case hipArgReadOnly:
+                // emum for acc qual type in presil_helper
+                case READ_ONLY:
                     printf("   .access:\tread_only\n");
                     printf("   .value_kind:\tglobal_buffer\n");
                     break;
-                case hipArgWriteOnly:
+                case WRITE_ONLY:
                     printf("   .access:\twrite_only\n");
                     printf("   .value_kind:\tglobal_buffer\n");
                     break;
-                case hipArgReadWrite:
+                case READ_WRITE:
                     printf("   .access:\tread_write\n");
                     printf("   .value_kind:\tglobal_buffer\n");
                     break;
                 }
             }
         }
-
-        hip_error = hipFreeKernelData(&kernel_data);
+        // free the struct
+        kernel_data.free();
     }
 
     std::cout << "Ran using Pre-Sil HIP .so!\n";
