3b31b519ae129293b5a1bc153c620a61b380f813
Abhishek Mishra <abhishek.mishra2@amd.com>
2024-12-12 16:56:56 +0000

    adding flags to use simulator (e.g., ffm)

--- rocclr/utils/flags.hpp
+++ rocclr/utils/flags.hpp
@@ -272,6 +272,10 @@ release(bool, DEBUG_CLR_KERNARG_HDP_FLUSH_WA, false,                          \
 release(uint, DEBUG_HIP_7_PREVIEW, 0,                                         \
         "Enables specific backward incompatible changes support before 7.0,"  \
         "using the mask. By default the changes are disabled and is set to 0")\
+release(uint, HIP_USE_SIM, -1,                                                \
+        "Bypass silicon to use simulator; 0 for FFM")                         \
+release(cstring, HIP_SIM_ARCH, "",                                            \
+        "Set arch for simulator (e.g., gfx1100)")                             \
 
 namespace amd {
 
